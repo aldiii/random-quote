e6f6b00a91fc24bb2296f9199a5f512809e97b43
Aldona Tomzik <a.tomzik@gmail.com>
2021-12-12 20:02:27 +0100

    Updates

---
+++ precache-manifest.b350e7f6205e5b36f8aae4787a506d18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ff5daaffed2ae0e7f929ee654af21d",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "9a0dda8334334247242f",
+    "url": "/random-quote/static/css/main.a59f2d47.chunk.css"
+  },
+  {
+    "revision": "16c43572504dd0cafe7f",
+    "url": "/random-quote/static/js/2.28967587.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/random-quote/static/js/2.28967587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24536b31eb7f2c77fa61",
+    "url": "/random-quote/static/js/3.00202d9c.chunk.js"
+  },
+  {
+    "revision": "9a0dda8334334247242f",
+    "url": "/random-quote/static/js/main.f21943b6.chunk.js"
+  },
+  {
+    "revision": "471c12208cabfd01a929",
+    "url": "/random-quote/static/js/runtime-main.6d2ef5c5.js"
+  },
+  {
+    "revision": "a6eca6bd65be479bf382819ce8bbbdb3",
+    "url": "/random-quote/static/media/logo.a6eca6bd.svg"
+  }
+]);
